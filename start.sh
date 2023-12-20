98b89a0933c9501ba63ccc306332d31380b62235
Lars Riel <larsriel@gmail.com>
2023-12-20 04:07:17 +0100

    fixed bug in deployment config

--- start.sh
+++ start.sh
@@ -1,13 +1,13 @@
 docker build -t rpg_server:latest .
 
 docker network rm rpg_network > /dev/null 2>&1
-docker network create rpg_network
+docker network create rpg_net
 docker network connect rpg_net nginx-proxy > /dev/null 2>&1
 
 docker stop rpg_mariadb > /dev/null 2>&1
 docker rm rpg_mariadb > /dev/null 2>&1
 docker run --name rpg_mariadb \
-    --net rpg_network \
+    --net rpg_net \
     -v ./data/mariadb:/var/lib/mysql:rw \
     -e MARIADB_ROOT_PASSWORD=password \
     -e MARIADB_DATABASE=database \
@@ -18,14 +18,14 @@ docker run --name rpg_mariadb \
 docker stop rpg_surrealdb > /dev/null 2>&1
 docker rm rpg_surrealdb > /dev/null 2>&1
 docker run --name rpg_surrealdb \
-    --net rpg_network \
+    --net rpg_net \
     -v ./data/surrealdb:/data:rw \
     -d surrealdb/surrealdb:latest start --user root --pass root file:/data/mydatabase.db
 
 docker stop rpg_server > /dev/null 2>&1
 docker rm rpg_server > /dev/null 2>&1
 docker run -d --name rpg_server \
-    --net rpg_network \
+    --net rpg_net \
     -p 3000:3000 \
     -e "VIRTUAL_HOST=rpg.treeleaf.dev" \
     -e "VIRTUAL_PORT=3000" \
--- stop.sh
+++ stop.sh
@@ -4,4 +4,5 @@ docker stop rpg_mariadb > /dev/null 2>&1
 docker rm rpg_mariadb
 docker stop rpg_surrealdb > /dev/null 2>&1
 docker rm rpg_surrealdb
-docker network rm rpg_network
+docker network disconnect rpg_net nginx-proxy > /dev/null 2>&1
+docker network rm rpg_net
